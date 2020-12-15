4cbb36020ff201515a7194785e521b060a768781
iamtimleonard <timleonardcello@gmail.com>
2020-12-15 15:50:08 -0500

    refactor text element creation

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ async function getWeek(year, week, team) {
 
 //Function to create a cell for season data
 const createCell = (stat, data, home, win) => {
-    const season = document.querySelector(".season");
     const cell = document.createElement("td");
     const {
         week,
@@ -45,8 +44,6 @@ const createCell = (stat, data, home, win) => {
     cell.textContent = stat;
     cell.style.cursor = "pointer";
     cell.classList.add("item");
-    cell.classList.add(week);
-    cell.classList.add(win ? "win" : "loss");
     cell.addEventListener("click", async function (e) {
         const searchTeam = home ? home_team : away_team;
         const [teamData] = await getWeek(inputContent.year, week, searchTeam);
@@ -58,7 +55,35 @@ const createCell = (stat, data, home, win) => {
         gameMatchup(data, teamData, home);
         document.querySelector(".details").classList.remove("hidden");
     });
-    season.appendChild(cell);
+    return cell;
+}
+
+const createRow = (data, home, win, cells) => {
+    const row = document.createElement("tr");
+    for (const cell of cells) {
+        const newCell = createCell(cell, data, home, win);
+        row.appendChild(newCell)
+    }
+    row.classList.add(win ? "win" : "loss");
+    return row;
+}
+
+const createTextElement = (type, content, parent) => {
+    const element = document.createElement(type);
+    element.textContent = content;
+    parent.appendChild(element);
+}
+
+//format UTC date
+const formatDate = (date) => {
+    const formattedDate = new Date(date)
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+    return formattedDate.toLocaleDateString(undefined, options)
 }
 
 //Populate single game matchup stats
@@ -78,14 +103,6 @@ const gameMatchup = (data, details, homeGame) => {
         away_line_scores
     } = data;
 
-    const date = new Date(start_date)
-    const options = {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-    };
-
     const {
         opponent
     } = details
@@ -95,61 +112,37 @@ const gameMatchup = (data, details, homeGame) => {
     const gameSummary = document.createElement("div");
     gameSummary.classList.add("summary");
     detailsSection.appendChild(gameSummary);
+    //score heading
+    createTextElement("h3", `${home_team} - ${home_points}, ${away_team} - ${away_points}`, gameSummary);
+    //date heading
+    createTextElement("h4", formatDate(start_date), gameSummary);
+    //home team heading
+    createTextElement("p", `${home_team} at home`, gameSummary);
+    //win/loss heading
+    let resultsContent = (homeGame && home_points > away_points) || (!homeGame && away_points > home_points) ? `${searchedTeam} Victory` : `${searchedTeam} Loss`;
+    createTextElement("p", resultsContent, gameSummary);
 
-    const summaryHeading = document.createElement("h3");
-    summaryHeading.textContent = `${home_team} - ${home_points}, ${away_team} - ${away_points}`;
-    gameSummary.appendChild(summaryHeading);
-
-    const dateHeading = document.createElement("h4");
-    dateHeading.textContent = date.toLocaleDateString(undefined, options);
-    gameSummary.appendChild(dateHeading);
-
-    const scoreSummary = document.createElement("p");
-    scoreSummary.textContent = homeGame ? `${home_team} at home` : `${away_team} away`
-    gameSummary.appendChild(scoreSummary);
-
-    const resultHeading = document.createElement("p");
-    resultHeading.textContent = (homeGame && home_points > away_points) || (!homeGame && away_points > home_points) ? `${searchedTeam} Victory` : `${searchedTeam} Loss`;
-    gameSummary.appendChild(resultHeading);
-    //table showing scores for each quarter
     const scoreTable = document.createElement("table");
     scoreTable.classList.add("score-table")
     const tableHeading = document.createElement("thead");
     scoreTable.appendChild(tableHeading);
-    const quarterHeading = document.createElement("th");
-    quarterHeading.textContent = `Quarter`;
-    tableHeading.appendChild(quarterHeading);
-    const homeHeading = document.createElement("th");
-    homeHeading.textContent = `${home_team}`;
-    tableHeading.appendChild(homeHeading);
-    const awayHeading = document.createElement("th");
-    awayHeading.textContent = `${away_team}`;
-    tableHeading.appendChild(awayHeading);
+    createTextElement("th", `Quarter`, tableHeading);
+    createTextElement("th", `${home_team}`, tableHeading);
+    createTextElement("th", `${away_team}`, tableHeading);
     gameSummary.appendChild(scoreTable);
+    //makes score table
     for (let i = 1; i < 5; i++) {
         const row = document.createElement("tr")
         scoreTable.appendChild(row);
-        const quarter = document.createElement("td");
-        quarter.textContent = `${i}`;
-        row.appendChild(quarter);
-        const homeScore = document.createElement("td");
-        homeScore.textContent = `${home_line_scores[i-1]}`;
-        row.appendChild(homeScore);
-        const awayScore = document.createElement('td');
-        awayScore.textContent = `${away_line_scores[i-1]}`;
-        row.appendChild(awayScore);
+        createTextElement("td", `${i}`, row);
+        createTextElement("td", `${home_line_scores[i-1]}`, row);
+        createTextElement("td", `${away_line_scores[i-1]}`, row);
     }
     const totalsRow = document.createElement("tr");
     scoreTable.appendChild(totalsRow);
-    const totalText = document.createElement("td");
-    totalText.textContent = `Total`;
-    totalsRow.appendChild(totalText);
-    const homeTotal = document.createElement("td");
-    homeTotal.textContent = `${home_points}`;
-    totalsRow.appendChild(homeTotal);
-    const awayTotal = document.createElement("td");
-    awayTotal.textContent = `${away_points}`;
-    totalsRow.appendChild(awayTotal);
+    createTextElement("td", "Total", totalsRow);
+    createTextElement("td", `${home_points}`, totalsRow);
+    createTextElement("td", `${away_points}`, totalsRow);
     //option to search matchup history
     const searchMatchup = document.createElement("button");
     searchMatchup.textContent = "See matchup history";
@@ -180,15 +173,9 @@ const makeMatchupHistory = (data) => {
     matchupHistory.classList.add("summary")
     const detailsSection = document.querySelector(".details")
     detailsSection.appendChild(matchupHistory);
-    const matchupHeading = document.createElement("h3");
-    matchupHeading.textContent = `${team1} vs. ${team2} Matchup history`
-    matchupHistory.appendChild(matchupHeading);
-    const record = document.createElement("p");
-    record.textContent = `${team1} has ${team1Wins} wins, ${team2Wins} losses, and ${ties} ties against ${team2}`;
-    matchupHistory.appendChild(record);
-    const fiveYearText = document.createElement("p");
-    fiveYearText.textContent = `Past five games:`
-    matchupHistory.appendChild(fiveYearText);
+    createTextElement("h3", `${team1} vs. ${team2} Matchup history`, matchupHistory);
+    createTextElement("p", `${team1} has ${team1Wins} wins, ${team2Wins} losses, and ${ties} ties against ${team2}`, matchupHistory);
+    createTextElement("p", "Past five games:", matchupHistory);
     //get five previous game scores, if available
     for (let i = games.length > 5 ? games.length - 5 : 0; i < games.length; i++) {
         const {
@@ -197,9 +184,7 @@ const makeMatchupHistory = (data) => {
             season,
             winner
         } = games[i];
-        const gameDetail = document.createElement("p");
-        gameDetail.textContent = `${season}: ${homeScore} - ${awayScore}, ${winner} win`;
-        matchupHistory.appendChild(gameDetail);
+        createTextElement("p", `${season}: ${homeScore} - ${awayScore}, ${winner} win`, matchupHistory);
     };
 }
 
@@ -207,7 +192,6 @@ const makeMatchupHistory = (data) => {
 const populateSeason = (season) => {
     console.log(season);
     for (let game of season) {
-        const season = document.querySelector(".season")
         const {
             week,
             away_team,
@@ -217,15 +201,8 @@ const populateSeason = (season) => {
         } = game;
         const homeGame = home_team === inputContent.school ? true : false;
         const win = (homeGame && home_points > away_points) || (!homeGame && away_points > home_points) ? true : false;
-        const row = document.createElement("tr");
-        season.appendChild(row);
-        row.classList.add("item");
-        row.setAttribute("id", week);
-        createCell(week, game, homeGame, win);
-        createCell(home_team, game, homeGame, win);
-        createCell(away_team, game, homeGame, win);
-        createCell(home_points, game, homeGame, win);
-        createCell(away_points, game, homeGame, win);
+        const row = createRow(game, homeGame, win, [week, home_team, away_team, home_points, away_points]);
+        document.querySelector(".season").appendChild(row);
     };
 };
 
